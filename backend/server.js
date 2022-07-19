909e7019ee5e54fd1308d9af89ea482313a1b179
rajpaul8 <rajpaul83@gmail.com>
2022-07-20 05:14:27 +0530

    Error and exception Handlers added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 const { setRandomFallback } = require("bcryptjs");
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.get("/", (req, res) =>
   res.json({ message: "Welcome to the Support Desk API"})
 );
+
+// **** Routes ****
+// User routes
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+
+
+// Middleware
+
+app.use(errorHandler);
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
