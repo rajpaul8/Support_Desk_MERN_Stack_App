539793e78c89d71c7eff567f21bc10e834a5f10b
rajpaul8 <rajpaul83@gmail.com>
2022-07-20 04:33:43 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const { setRandomFallback } = require("bcryptjs");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) =>
+  res.json({ message: "Welcome to the Support Desk API"})
+);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
