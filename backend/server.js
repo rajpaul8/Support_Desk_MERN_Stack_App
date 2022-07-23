4a95126caa923f3770d9ef96e74425a4b93cbb3f
rajpaul8 <rajpaul83@gmail.com>
2022-07-23 22:06:22 +0530

    added heroku postbuild config

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB  =require('./config/db')
 const PORT = process.env.PORT || 5000;
+const path = require('path')
 var cors = require("cors");
 
 
@@ -20,19 +21,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.get("/", (req, res) =>
-  res.json({ message: "Welcome to the Support Desk API"})
-);
-
 // **** Routes ****
 // User routes
 app.use('/api/users', require('./routes/userRoutes'))
 // Ticket routes
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// Middleware
+// Serve Production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req,res)=> res.sendFile(__dirname,'../','frontend', 'build', index.html))
+} else {
+  app.get("/", (req, res) =>
+    res.json({ message: "Welcome to the Support Desk API" })
+  );
+}
+  // Middleware
 
-app.use(errorHandler);
+  app.use(errorHandler);
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
